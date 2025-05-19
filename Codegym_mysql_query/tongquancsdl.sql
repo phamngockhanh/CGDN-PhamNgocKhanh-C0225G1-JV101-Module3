2e0c114006152dd77d77d2f568d0947411e18008
phamngockhanh <ngockhanhpham8a@gmail.com>
2025-05-19 14:21:32 +0700

    sql

---
+++ Codegym_mysql_query/tongquancsdl.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE `c0225g1` ;
+
+use c0225g1;
+
+create table Class(
+id int primary key,
+`name` varchar(30)
+);
+
+create table teacher(
+id int primary key,
+`name` varchar(30),
+age int,
+country varchar(30)
+);
+
